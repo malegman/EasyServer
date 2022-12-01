3382f28d45be21fd7e8b6399d31981ccd84ab546
Maximov Oleg <smegovic@gmail.com>
2022-12-01 19:04:23 +0500

    Доработка валидации

--- src/main/java/com/example/easyserver/validation/Validation.java
+++ src/main/java/com/example/easyserver/validation/Validation.java
@@ -1,16 +1,23 @@
 package com.example.easyserver.validation;
 
+import lombok.EqualsAndHashCode;
+
 import java.util.*;
 
 /**
  * Валидатор для регистрации нарушений
  */
+@EqualsAndHashCode(of = "violations")
 public class Validation {
 
     private final Map<String, List<Violation>> violations;
 
     public Validation() {
         this.violations = new HashMap<>();
+    }
+
+    public Validation(final Map<String, List<Violation>> violations) {
+        this.violations = Objects.requireNonNull(violations);
     }
 
     public void registerViolation(String property, String errorMessage) {
